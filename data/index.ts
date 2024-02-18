475bdc616f812124650cc933e3f19b07fc98825f
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-02-18 19:18:18 +0800

    Refactor product list import

---
+++ data/index.ts
@@ -0,0 +1,85 @@
+export const products = [
+  {
+    title: "NextGig",
+    link: "https://nextgig-app.vercel.app",
+    thumbnail: "/projects/nextgig.png",
+  },
+  {
+    title: "Astro Foundation",
+    link: "https://www.astro-foundation.com",
+    thumbnail: "/projects/astro-foundation.png",
+  },
+  {
+    title: "WatchBox",
+    link: "https://watchbox.vercel.app",
+    thumbnail: "/projects/watchbox.png",
+  },
+  {
+    title: "NextSched",
+    link: "https://nextsched-beta.vercel.app",
+    thumbnail: "/projects/nextsched.png",
+  },
+  {
+    title: "Connect",
+    link: "https://editrix.ai",
+    thumbnail: "/projects/connect.png",
+  },
+  {
+    title: "NextCrew - Admin",
+    link: "",
+    thumbnail: "/projects/nextcrew-admin.png",
+  },
+  {
+    title: "NextCrew - Portal",
+    link: "",
+    thumbnail: "/projects/nextcrew-portal.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  },
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  },
+];
